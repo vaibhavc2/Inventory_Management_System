a87faaa1a620d13ce992177376f87869b02d063b
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-11-17 10:51:06 +0530

    update details: heading

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inventory-Management-System</title>
+    <title>Shop-Management-System</title>
 
     <!-- Font Icon -->
     <link rel="stylesheet" href="{{ asset('frontend') }}/fonts/material-icon/css/material-design-iconic-font.min.css">
