89bc23516bf4c662d5ceb519cbe7d57cef77e6f6
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-11-17 10:37:53 +0530

    update details: heading

--- resources/views/layouts/admin_master.blade.php
+++ resources/views/layouts/admin_master.blade.php
@@ -6,7 +6,7 @@ Products<!DOCTYPE html>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>InventoryManagementSystem</title>        
+        <title>Shop Management</title>        
 
         <link href="{{ asset('backend') }}/css/styles.css" rel="stylesheet" />
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
